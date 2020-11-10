5b88157e689a548ee06a168f29caefc52c3bdca0
NGowthamKumar <gowthamkumar21011999@gmail.com>
2020-11-10 17:26:29 +0530

    updated

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -3,5 +3,5 @@ import {mongodbSuccess, mongodbError} from '../constants/constants';
 
 mongoose.connect(process.env.MONGO_DB, process.env.MONGO_JSON, function(err) {
   if (err) console.log(mongodbError);
-  else console.log(mongodbSuccess)
+  else console.log(mongodbSuccess);
 });
